4dee034b60985283bdaa33d26f3ebb11dc940b66
TihrLef <shkaf@nevalera.ru>
2021-03-29 01:34:53 +0300

    add poz, <=, >=

--- ll.cpp
+++ ll.cpp
@@ -58,25 +58,45 @@ bool operator <(const ll& lval, const T& rval)
 }
 
 bool operator ==(const ll &lval, const ll &rval){
-
     if (lval.sign != rval.sign){
         return false;// не равны
-    }
     else{
         unsigned int len1 = lval.num.size();
         if (len1 != rval.num.size()) return false; // не равны
-        else{
-            for (unsigned int i = 0; i<len1; i++){
-                if (lval.num[i] != rval.num[i]){
+        else
+            for (unsigned int i = 0; i<len1; i++)
+                if (lval.num[i] != rval.num[i])
                     return false;
-                }
-            }
             return true;
-        }
     }
-
 }
 
 bool operator !=(const ll& lval, const ll& rval){
     return !(lval == rval);
+}
+
+template<typename T> //объ€вление параметра шаблона функции
+T operator >= (const T& lval, const ll& rval)
+{   return !(lval>rval);
+}
+
+template<typename T>
+T operator >= (const T& lval, const ll& rval)
+{   return !(lval<rval);
+}
+
+template<typename T>
+T operator <= (const T& lval, const ll& rval)
+{   return !(lval>rval);
+}
+
+template<typename T>
+T operator <= (const T& lval, const ll& rval)
+{   return !(lval>rval);
+}
+
+short ll::poz()
+{   if (this->num[0] == 0) return 0;
+    else if (this->sign) return 1;
+    else if (!this->sign) return 2;
 }
