007a797e51de6348564d3995006ef4f70148a4e6
Ilya.Tsygankov <saw44947@gmail.com>
2021-03-29 22:41:17 +0300

    Add constructor for string(ver1.1)

--- ll.cpp
+++ ll.cpp
@@ -17,4 +17,13 @@ ll::ll(long long to): sign{ to < 0 }{
     num.push_back(to % power::mod);
     num.push_back(to / power::mod);
 };
+ll::ll(std::string to){
+    long long digit = std::stoll(to);
+    sign = (digit<0)? true:false; 
+    if(sign) digit *= -1;
+    num.push_back(digit % power::mod);
+    digit /= power::mod;
+    num.push_back(digit % power::mod);
+    num.push_back(digit / power::mod);
+}
 ll::ll(bool sigN, std::vector<long long> nuM): sign{ sigN }, num{ nuM }{};
