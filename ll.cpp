9a2cb487f3f0dacb29732b99a133895bf309825b
TihrLef <shkaf@nevalera.ru>
2021-03-30 21:59:59 +0300

    microfix

--- ll.cpp
+++ ll.cpp
@@ -1,6 +1,5 @@
 #include "ll.h"
 
-<<<<<<< HEAD
 /**\brief оператор > дл€ двух ll чисел
  * —равнивает последовательно сначала знаки чисел, потом их длину, потом начина€ с наибольшего сравнивает их посимвольно
  * Ќе зависит от других функций и операторов
@@ -16,7 +15,7 @@ bool operator >(const ll& lval, const ll& rval)
     auto lsize=lval.num.size();
     if (lsize!=rval.num.size())
         return ((lsize>rval.num.size())^lval.sign);
-    for (int i=0; i<lsize; i++)
+    for (int i=lsize-1; i>-1; i--)
         if (lval.num[i]!=rval.num[i])
             return (lval.num[i]>rval.num[i])^lval.sign;
     return 0;
@@ -59,16 +58,17 @@ bool operator <(const ll& lval, const T& rval)
 }
 
 bool operator ==(const ll &lval, const ll &rval){
-    if (lval.sign != rval.sign){
+    if (lval.sign != rval.sign)
         return false;// не равны
     else{
         unsigned int len1 = lval.num.size();
         if (len1 != rval.num.size()) return false; // не равны
-        else
-            for (unsigned int i = 0; i<len1; i++)
+        else{
+            for (unsigned int i = len1-1; i>-1; i--)
                 if (lval.num[i] != rval.num[i])
                     return false;
             return true;
+        }
     }
 }
 
@@ -99,33 +99,5 @@ T operator <= (const T& lval, const ll& rval)
 short ll::poz()
 {   if (this->num[0] == 0) return 0;
     else if (this->sign) return 1;
-    else if (!this->sign) return 2;
-=======
-namespace power{
-    const int mod = 1000000000;
+    else return 2;
 }
-
-ll::ll(): sign{ false }, num{ 0 }{};
-ll::ll(int to): sign{ to < 0 }{
-    if (sign) to *= -1;
-    num.push_back(to % power::mod);
-    num.push_back(to / power::mod);
-};
-ll::ll(long long to): sign{ to < 0 }{
-    if (sign) to *= -1;
-    num.push_back(to % power::mod);
-    to /= power::mod;
-    num.push_back(to % power::mod);
-    num.push_back(to / power::mod);
-};
-ll::ll(std::string to){
-    long long digit = std::stoll(to);
-    sign = (digit<0)? true:false; 
-    if(sign) digit *= -1;
-    num.push_back(digit % power::mod);
-    digit /= power::mod;
-    num.push_back(digit % power::mod);
-    num.push_back(digit / power::mod);
->>>>>>> 007a797e51de6348564d3995006ef4f70148a4e6
-}
-ll::ll(bool sigN, std::vector<long long> nuM): sign{ sigN }, num{ nuM }{};
