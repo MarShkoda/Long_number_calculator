61d161861972cdc7110e9b8f3a043bf226b26ae8
Ilya.Tsygankov <saw44947@gmail.com>
2021-04-09 22:02:33 +0300

    Fix problems with insert

--- ll.cpp
+++ ll.cpp
@@ -7,15 +7,15 @@ namespace power{
 ll::ll(): sign{ false }, num{ 0 }{};
 ll::ll(int to): sign{ to < 0 }{
     if (sign) to *= -1;
-    num.insert(num.begin(), to % power::mod);
-    num.insert(num.begin(), to / power::mod);
+    num.push_back(to % power::mod);
+    num.push_back(to / power::mod);
 };
 ll::ll(long long to): sign{ to < 0 }{
     if (sign) to *= -1;
-    num.insert(num.begin(), to % power::mod);
+    num.push_back(to % power::mod);
     to /= power::mod;
-    num.insert(num.begin(), to % power::mod);
-    num.insert(num.begin(), to / power::mod);
+    num.push_back(to % power::mod);
+    num.push_back(to / power::mod);
 };
 ll::ll(std::string to){
     std::string tmp_string = to.substr(to.length()-20, 21);
@@ -23,20 +23,22 @@ ll::ll(std::string to){
     long long digit = std::stoll(to);
     sign = (digit<0)? true:false;
     if (sign) digit *= -1;
-    num.insert(num.begin(), digit % power::mod);
+    num.push_back(digit % power::mod);
     digit /= power::mod;
-    num.insert(num.begin(), digit % power::mod);
-    num.insert(num.begin(), digit / power::mod);
+    num.push_back(digit % power::mod);
+    num.push_back(digit / power::mod);
 };
 ll::ll(bool sigN, std::vector<long long> nuM): sign{ sigN }, num{ nuM }{};
 
 int main(void)
 {
+    // Just test for outputting
     std::string digit_str = "-2233720368547758082";
     ll digit = ll(digit_str);
     if(digit.getSign()) std::cout << '-';
     for(long long element: digit.getVec()){
         std::cout << element;
     }
+    // Just test for outputting
     return 0;
 }
